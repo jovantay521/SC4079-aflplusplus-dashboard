3b50ac9f0140301841cb11f22dbfcc6404a7a7c6
Jovan <jovantay521@gmail.com>
2025-12-16 16:41:32 +0800

    refactor: update queue page to use new load_plot_data

--- queue.py
+++ queue.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime
 import time
 import logging
+import glob
+import os
+from utils import *
 
 # logging configuration
 logging.basicConfig(
@@ -16,63 +20,60 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 UPDATE_INTERVAL = 60
-
-
-queue_data_filepath = 'sample-data/main/queue_data'
-plot_data_filepath = 'sample-data/main/plot_data'
+DATA_DIRECTORY_PATH = 'sample-data'
 
 st.set_page_config(layout='wide')
-# @st.cache_data(ttl=60)
-# def load_queue_data(file_path):
-#     df = pd.read_csv(file_path)
-#     df.columns = df.columns.str.strip()
-#     df.rename(columns={df.columns[0]: 'filename'}, inplace=True)
-#     return df
-
-
-def load_plot_data(file_path, skip_rows=0):
-    start_time = time.time()
-    if skip_rows > 0:
-        df = pd.read_csv(file_path, skiprows=range(1, skip_rows + 1))
-    else:
-        df = pd.read_csv(file_path)
-    df.columns = df.columns.str.strip()
-    df.rename(columns={df.columns[0]: 'relative_time'}, inplace=True)
-    logger.info(
-        f"load_plot_data,{time.time() - start_time},skip_rows={skip_rows}")
-    return df
 
 
 if st.button("Refresh") or 'plot_data' not in st.session_state:
-    st.session_state.plot_data = load_plot_data(plot_data_filepath)
-    st.session_state.plot_data_len = len(st.session_state.plot_data)
+    for file_path in glob.glob(os.path.join(DATA_DIRECTORY_PATH, "*", "plot_data")):
+        base_name = os.path.basename(os.path.dirname(file_path))
+        plot_data = load_plot_data(file_path)
+        st.session_state[base_name] = {
+            'plot_data': plot_data,
+            'plot_data_len': len(plot_data)
+        }
 
 
 @st.fragment(run_every=UPDATE_INTERVAL)
 def generate_chart():
-    start_time = time.time()
-    new_data = load_plot_data(
-        plot_data_filepath, skip_rows=st.session_state.plot_data_len)
-    if not new_data.empty:
-        st.session_state.plot_data = pd.concat(
-            [st.session_state.plot_data, new_data], ignore_index=True)
-    st.session_state.plot_data_len = len(st.session_state.plot_data)
-    fig = px.line(
-        st.session_state.plot_data,
-        x='relative_time',
-        y=['corpus_count', 'pending_total', 'pending_favs'],
-        labels={'value': 'Count', 'variable': 'Metric',
-                'relative_time': 'Time'},
-        title="Queue Metrics Over Time",
-        hover_data=['cur_item']
-    )
-
-    st.plotly_chart(fig, width='stretch')
-    st.caption(
-        f"**Last updated:** {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (Update interval: {UPDATE_INTERVAL}s)")
-    if st.checkbox('Show latest data'):
-        st.dataframe(new_data.tail())
-    logger.info(f"generate_chart,{time.time() - start_time}")
+    # update session plot data
+    for file_path in glob.glob(os.path.join(DATA_DIRECTORY_PATH, "*", "plot_data")):
+        base_name = os.path.basename(os.path.dirname(file_path))
+        session_plot_data = st.session_state[base_name]['plot_data']
+        session_plot_data_len = st.session_state[base_name]['plot_data_len']
+        new_plot_data = load_plot_data(
+            file_path, skip_rows=session_plot_data_len)
+        if not new_plot_data.empty:
+            updated_plot_data = pd.concat(
+                [session_plot_data, new_plot_data], ignore_index=True)
+            st.session_state[base_name] = {
+                'plot_data': updated_plot_data,
+                'plot_data_len': len(updated_plot_data)
+            }
+    metrics = ['corpus_count', 'pending_total', 'pending_favs']
+    metric_titles = {
+        'corpus_count': 'Corpus Count Over Time',
+        'pending_total': 'Pending Total Over Time',
+        'pending_favs': 'Pending Favs Over Time'
+    }
+    cols = st.columns(3, border=True)
+    for idx, metric in enumerate(metrics):
+        fig = go.Figure()
+        for file_path in glob.glob(os.path.join(DATA_DIRECTORY_PATH, "*", "plot_data")):
+            base_name = os.path.basename(os.path.dirname(file_path))
+            session_plot_data = st.session_state[base_name]['plot_data']
+            fig.add_trace(go.Scatter(
+                x=session_plot_data['relative_time'],
+                y=session_plot_data[metric],
+                name=base_name,
+            ))
+        fig.update_layout(title=metric_titles[metric])
+        with cols[idx]:
+            st.plotly_chart(fig)
 
 
 generate_chart()
+
+st.caption(
+    f"**Last updated:** {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (Update interval: {UPDATE_INTERVAL}s)")
